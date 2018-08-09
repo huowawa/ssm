3997bce0d0c23ac1a563c5750552f5dd1b31001f
火娃娃 <1608692339@qq.com>
2018-08-09 22:56:17 +0800

    修改了UserController

--- src/main/java/com/soft/platform/system/controller/SysUserController.java
+++ src/main/java/com/soft/platform/system/controller/SysUserController.java
@@ -6,19 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/user")
 public class SysUserController {
 
     @Autowired
     private SysUserService sysUserService;
 
-    @RequestMapping("/all")
-    @ResponseBody
+   @RequestMapping("/all")
     public List<SysUser> findAllUsers(HttpServletRequest request){
         return sysUserService.findAllUsers();
     }
